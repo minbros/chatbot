ccab664f4a763bcc10c29a355789697d7e35a8b6
minbros <pristo24631@gmail.com>
2024-03-18 18:19:08 +0900

    Initial commit

---
+++ lib/config/palette.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const primary = Color(0xFF6581C9);
+
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,23 @@
+import 'package:chatbot/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: true,
+        backgroundColor: Colors.blue,
+        title: Text('Chatbot'),
+      ),
+      body: const ChatScreen(),
+    );
+  }
+}
